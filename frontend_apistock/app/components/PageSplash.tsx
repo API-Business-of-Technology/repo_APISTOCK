32ba3acc215c340f92601e6b4bbb51bbc373272e
Pierre Richard Saint Louis <saintlouispierrerichard6@gmail.com>
2026-08-24 15:16:21 -0400

    Developpement de style de la page splash.

---
+++ frontend_apistock/app/components/PageSplash.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+import "./PageSplash.css";
+
+export default function PageSplash(){
+
+    return(
+
+        <div className="containersplash">
+
+        </div>
+    );
+}
