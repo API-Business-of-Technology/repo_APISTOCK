15954af3997481ff46a0f21fdfa0e0885ff45e90
Pierre Richard Saint Louis <saintlouispierrerichard6@gmail.com>
2026-09-02 20:09:07 -0400

    Modification de package json

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -7,3 +8,25 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
 ]);
+
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import eslintParserTs from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: eslintParserTs,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": eslintPluginTs,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+];
+
