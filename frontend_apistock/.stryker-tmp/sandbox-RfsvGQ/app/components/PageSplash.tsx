1bd38ca765296baa223269c09ea6308882448866
Saint Louis Pierre Richard <apibusinesstechnology@gmail.com>
2026-09-04 14:47:03 -0400

    Delete frontend_apistock/.stryker-tmp/sandbox-RfsvGQ/app directory
    Suppression du dossier frontend du repo

--- frontend_apistock/.stryker-tmp/sandbox-RfsvGQ/app/components/PageSplash.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-"use client";
-import Image from "next/image";
-import "./PageSplash.css";
-
-export default function PageSplash() {
-  return <div className="containersplash"></div>;
-}
